2a88370e0463b660a7ce789b94177ca36b1ddd65
germaine <germaine5152seah@gmail.com>
2021-09-19 14:44:22 +0800

    Fix CheckStyle issues

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARK_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARK_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
@@ -22,6 +23,9 @@ import seedu.address.model.person.Person;
 import seedu.address.model.person.Remark;
 import seedu.address.testutil.PersonBuilder;
 
+/**
+ * Contains tests for RemarkCommand
+ */
 class RemarkCommandTest {
 
     private static final String REMARK_STUB = "Some remark";
@@ -104,19 +108,25 @@ class RemarkCommandTest {
     public void equals() {
         final RemarkCommand standardCommand = new RemarkCommand(INDEX_FIRST_PERSON,
                 new Remark(VALID_REMARK_AMY));
+
         // same values -> returns true
         RemarkCommand commandWithSameValues = new RemarkCommand(INDEX_FIRST_PERSON,
                 new Remark(VALID_REMARK_AMY));
         assertTrue(standardCommand.equals(commandWithSameValues));
+
         // same object -> returns true
         assertTrue(standardCommand.equals(standardCommand));
+
         // null -> returns false
         assertFalse(standardCommand.equals(null));
+
         // different types -> returns false
         assertFalse(standardCommand.equals(new ClearCommand()));
+
         // different index -> returns false
         assertFalse(standardCommand.equals(new RemarkCommand(INDEX_SECOND_PERSON,
                 new Remark(VALID_REMARK_AMY))));
+
         // different remark -> returns false
         assertFalse(standardCommand.equals(new RemarkCommand(INDEX_FIRST_PERSON,
                 new Remark(VALID_REMARK_BOB))));
